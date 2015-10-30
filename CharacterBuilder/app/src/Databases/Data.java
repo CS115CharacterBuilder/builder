8899e417589e8ab62ea94e444cec6c9bf0f3ec84
Andrew Miller <anmcmill@ucsc.edu>
2015-10-30 14:58:01 -0700

    Dwarf Race and Subrace in racedata

--- CharacterBuilder/app/src/Databases/Data.java
+++ CharacterBuilder/app/src/Databases/Data.java
@@ -8,7 +8,18 @@ public class Data {
     String dClass; //character class
     String dRace; //character race
     String flavor; //the flavor text for the thing
+    String size; //the size of the character by race
+    String vision; //the type of eye sight the character has
+    String resistance; //any resistances the character has.
+    String weaponProf; //any weapon proficiencies the character gets from race.
+    String toolProf; //tool prof from race.
+    String armorProf; //armor...
+    String languages; //any languagese.
+    String misc1; //other stuff
     boolean has; //character has this data object
     boolean prof; //character is proficient in this
-    int statBoost[]; //any ability modifier
+    int statBoost[]; //{str,dex,con,int,wis,cha} any ability modifier
+    int speed; //the speed of the character by race.
+
+
 }
--- CharacterBuilder/app/src/Databases/RaceData.java
+++ CharacterBuilder/app/src/Databases/RaceData.java
@@ -10,7 +10,7 @@ public class RaceData extends Data {
 
     //Sample ability
     Data ability1 = new Data(); //declare a new ability object
-    public void setAbility1(Data ability1) { //populate it
+    public void setAbility1() { //populate it
         this.ability1 = ability1;
         ability1.abilName = "TestAbility";
         ability1.dClass = "Peasant";
@@ -22,10 +22,51 @@ public class RaceData extends Data {
         ability1.prof = true;
     }
 
+    Data hillDwarfSubrace = new Data();
+    public void setHillDwarfStatBoost(){
+        hillDwarfSubrace.abilName = "Hill Dwarf Ability Score Increase";
+        int stats[] = {0,0,0,0,1,0};
+        hillDwarfSubrace.statBoost = stats;
+        hillDwarfSubrace.misc1 = "Dwarven Toughness. Your hit point maximum increases by 1, and it increases by 1 every time you gain a level.";
+    }
+
+    Data mountianDwarfSubrace = new Data();
+    public void setMountianDwarfStatBoost(){
+        mountianDwarfSubrace.abilName = "Mountian Dwarf Ability Score Increase";
+        int stats[] = {0,0,0,0,1,0};
+        mountianDwarfSubrace.statBoost = stats;
+        mountianDwarfSubrace.armorProf = "Light Armor, Medium Armor";
+    }
+
+    Data dwarfTraits = new Data();
+    public void setDwarfTraits() {
+        dwarfTraits.abilName = "Dwarven Traits";
+        int stats[] = {0, 0, 2, 0, 0, 0};
+        dwarfTraits.statBoost = stats;
+        dwarfTraits.size = "Medium";
+        dwarfTraits.speed = 25;
+        dwarfTraits.vision = "Dark Vision:Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.";
+        dwarfTraits.resistance = "Poison. Save vs. Poison. Resistant to poison damage.";
+        dwarfTraits.weaponProf = "Battleaxe, Handaxe, Throwing Hammer, War Hammer";
+        dwarfTraits.toolProf =  "Smith Tools, Brewers Supplies, Mason's Tools";
+        dwarfTraits.languages = "Common, Dwarvish";
+    }
+
     //This method returns all the race data
-    public ArrayList<Data> getRd() {
-        setAbility1(ability1); //Be sure to set all abilities first
+    public ArrayList<Data> getRd(){
+        setAbility1(); //Be sure to set all abilities first
         rd.add(ability1);//Then add them to the Arraylist
+
+        setHillDwarfStatBoost();
+        rd.add(hillDwarfSubrace);
+
+        setMountianDwarfStatBoost();
+        rd.add(mountianDwarfSubrace);
+
+        setDwarfTraits();
+        rd.add(dwarfTraits);
+
+
         return rd;
     }
 }
