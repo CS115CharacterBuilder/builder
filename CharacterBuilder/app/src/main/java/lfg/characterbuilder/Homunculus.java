dc32b9ed12ecfe4d6ca58d53f07fd754e41f93e7
John Baden <jabaden@mac.com>
2015-11-14 12:41:23 -0800

    inexperienced questionnaire works
    its not finished, but it works.

--- CharacterBuilder/app/src/main/java/lfg/characterbuilder/Homunculus.java
+++ CharacterBuilder/app/src/main/java/lfg/characterbuilder/Homunculus.java
@@ -20,6 +20,7 @@ public class Homunculus{
     private int warlockScore;
     private int rogueScore;
     private int wizardScore;
+    private int clericScore;
 
     public Homunculus(){
         hRace = "";
@@ -39,6 +40,36 @@ public class Homunculus{
     }
     void setSubrace(String _subrace){
         hSubrace = _subrace;
+    }
+    void parseString(String aString){
+        if(aString.equals("Barbarian")){
+            this.barbarianScore++;
+        }
+        else if(aString.equals("Fighter")){
+            fighterScore++;
+        }
+        else if(aString.equals("Paladin")){
+            paladinScore++;
+        }
+        else if(aString.equals("Monk")){
+            monkScore++;
+        }
+        else if(aString.equals("Ranger")){
+            rangerScore++;
+        }
+        else if(aString.equals("Warlock")){
+            warlockScore++;
+        }
+        else if(aString.equals("Rogue")){
+            rogueScore++;
+        }
+        else if(aString.equals("Wizard")){
+            wizardScore++;
+        }
+        else if(aString.equals("Cleric")){
+            clericScore++;
+        }
+
     }
 
     String getRace(){
--- CharacterBuilder/app/src/main/java/lfg/characterbuilder/NoobQuestionnaire.java
+++ CharacterBuilder/app/src/main/java/lfg/characterbuilder/NoobQuestionnaire.java
@@ -27,6 +27,7 @@ public class NoobQuestionnaire extends AppCompatActivity implements AdapterView.
     ArrayList<NQTag> tagParseArray = new ArrayList<NQTag>();
 
     //ALL TAGS ARE CONTAINED HERE
+
     ArrayList<NQTag> tagArray = new ArrayList<NQTag>();
 
 
@@ -45,6 +46,14 @@ public class NoobQuestionnaire extends AppCompatActivity implements AdapterView.
         void printList(){
             for(int i = 0; i < this.tagList.size(); i++){
                 Log.d("tagList", this.tagList.get(i));
+            }
+        }
+        void iterateAndAdd(ArrayList<NQTag> arrayOfTags, Homunculus hom){
+            for(int i = 0; i < arrayOfTags.size(); i++){
+                for(int j = 0; j < arrayOfTags.get(i).tagList.size(); j++){
+                    //create a method in homunculus that parses the string
+                    hom.parseString(arrayOfTags.get(i).tagList.get(j));
+                }
             }
         }
     }
@@ -71,10 +80,7 @@ public class NoobQuestionnaire extends AppCompatActivity implements AdapterView.
 
 //----------------------------Tag 0.0---------------------------------------------------------
         ArrayList<String> testList = new ArrayList<String>();
-        testList.add("This");
-        testList.add("is");
-        testList.add("a");
-        testList.add("Test");
+        testList.add("Barbarian");
         NQTag test = new NoobQuestionnaire.NQTag(testList);
 
         tagArray.add(test);
@@ -82,46 +88,126 @@ public class NoobQuestionnaire extends AppCompatActivity implements AdapterView.
 
 //-----------------------------------------Tag 0.1--------------------------------------------------
         ArrayList<String> listOneTwo = new ArrayList<String>();
-        listOneTwo.add("test");
-        listOneTwo.add("for");
-        listOneTwo.add("listOneTwo");
-        listOneTwo.add("this is only a test");
+        listOneTwo.add("Fighter");
+        //listOneTwo.add("listOneTwo");
+        //listOneTwo.add("this is only a test");
         NQTag tagOneTwo = new NoobQuestionnaire.NQTag(listOneTwo);
 
         tagArray.add(tagOneTwo);
 //--------------------------------------------------------------------------------------------------
 //-----------------------------------------Tag 0.2--------------------------------------------------
         ArrayList<String> listZeroTwo = new ArrayList<String>();
-        listZeroTwo.add("test");
-        listZeroTwo.add("for");
-        listZeroTwo.add("listOneTwo");
-        listZeroTwo.add("this is only a test");
+        listZeroTwo.add("Monk");
         NQTag tagZeroTwo = new NoobQuestionnaire.NQTag(listZeroTwo);
 
         tagArray.add(tagZeroTwo);
 //--------------------------------------------------------------------------------------------------
 //-----------------------------------------Tag 0.3--------------------------------------------------
         ArrayList<String> listZeroThree = new ArrayList<String>();
-        listZeroThree.add("test");
-        listZeroThree.add("for");
-        listZeroThree.add("listOneTwo");
-        listZeroThree.add("this is only a test");
+        listZeroThree.add("Ranger");
         NQTag tagZeroThree = new NoobQuestionnaire.NQTag(listZeroThree);
 
         tagArray.add(tagZeroThree);
 //--------------------------------------------------------------------------------------------------
+
+//-----------------------------------------Tag 1.0--------------------------------------------------
+        ArrayList<String> listOneZero = new ArrayList<String>();
+        listOneZero.add("Ranger");
+        listOneZero.add("Bard");
+        listOneZero.add("Monk");
+        listOneZero.add("Rogue");
+        listOneZero.add("Elves");
+        listOneZero.add("Half-Elves");
+        listOneZero.add("Halflings");
+        NQTag tagOneZero = new NoobQuestionnaire.NQTag(listOneZero);
+
+        tagArray.add(tagOneZero);
+//-----------------------------------------Tag 1.0--------------------------------------------------
+//-----------------------------------------Tag 1.1--------------------------------------------------
+        ArrayList<String> listOneOne = new ArrayList<String>();
+        listOneOne.add("Fighter");
+        listOneOne.add("Barbarian");
+        listOneOne.add("Half-Orc");
+        listOneOne.add("Dwarf");
+        listOneOne.add("Dragonborn");
+        NQTag tagOneOne = new NoobQuestionnaire.NQTag(listOneOne);
+
+        tagArray.add(tagOneOne);
+//-----------------------------------------Tag 1.1--------------------------------------------------
+
+//-----------------------------------------Tag 2.0--------------------------------------------------
+        ArrayList<String> listTwoZero = new ArrayList<String>();
+        listTwoZero.add("Barbarian");
+        listTwoZero.add("Half-Orc");
+        listTwoZero.add("Dragonborn");
+        listTwoZero.add("Dwarf");
+
+        NQTag tagTwoZero = new NoobQuestionnaire.NQTag(listTwoZero);
+
+        tagArray.add(tagTwoZero);
+//-----------------------------------------Tag 2.0--------------------------------------------------
+
+//-----------------------------------------Tag 2.1--------------------------------------------------
+
+        ArrayList<String> listTwoOne = new ArrayList<String>();
+        listTwoOne.add("Fighter");
+        listTwoOne.add("Paladin");
+        listTwoOne.add("Monk");
+        listTwoOne.add("Dwarf");
+        listTwoOne.add("Human");
+
+        NQTag tagTwoOne = new NoobQuestionnaire.NQTag(listTwoOne);
+
+        tagArray.add(tagTwoOne);
+//-----------------------------------------Tag 2.1--------------------------------------------------
+
+//-----------------------------------------Tag 3.0--------------------------------------------------
+        //its better to be
+        //physically powerful
+        ArrayList<String> listThreeZero = new ArrayList<String>();
+        listThreeZero.add("Barbarian");
+        listThreeZero.add("Fighter");
+        listThreeZero.add("Half-Orc");
+        listThreeZero.add("Dragonborn");
+        listThreeZero.add("Dwarf");
+
+        NQTag tagThreeZero = new NoobQuestionnaire.NQTag(listThreeZero);
+
+        tagArray.add(tagThreeZero);
+
+//-----------------------------------------Tag 3.0--------------------------------------------------
+//-----------------------------------------Tag 3.1--------------------------------------------------
+        ArrayList<String> listThreeOne = new ArrayList<String>();
+        listThreeOne.add("Monk");
+        listThreeOne.add("Ranger");
+        listThreeOne.add("Rogue");
+        listThreeOne.add("Elf");
+        listThreeOne.add("Half-Elf");
+        listThreeOne.add("Tiefling");
+        listThreeOne.add("Halfling");
+        listThreeOne.add("Gnome");
+
+        NQTag tagThreeOne = new NoobQuestionnaire.NQTag(listThreeOne);
+
+        tagArray.add(tagThreeOne);
+//-----------------------------------------Tag 3.1--------------------------------------------------
+
+
+
         //displays the questions at the start
         displayQuestion(questionNumber, mainQuestion, choiceOne, choiceTwo, choiceThree, choiceFour, tagArray, tagParseArray);
 
         choiceOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(NoobQuestionnaire.this, "Ayy lmao, this is a test for the onClick", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(NoobQuestionnaire.this, "Ayy lmao, this is a test for the onClick", Toast.LENGTH_SHORT).show();
                 choiceOne.setText("clicked!");
                 NQTag tempTag = (NQTag) v.getTag();
-                tempTag.printList();
+                //tempTag.printList();
                 tagParseArray.add(tempTag);
-
+                for(int a = 0; a < tagParseArray.size(); a++){
+                  tagParseArray.get(a).printList();
+                }
                 displayQuestion(questionNumber, mainQuestion, choiceOne, choiceTwo, choiceThree, choiceFour, tagArray, tagParseArray);
 
             }
@@ -129,36 +215,41 @@ public class NoobQuestionnaire extends AppCompatActivity implements AdapterView.
         choiceTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(NoobQuestionnaire.this, "Ayy lmao, this is a test for the onClick", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(NoobQuestionnaire.this, "Ayy lmao, this is a test for the onClick", Toast.LENGTH_SHORT).show();
                 choiceTwo.setText("clicked!");
                 NQTag tempTag = (NQTag) v.getTag();
-                tempTag.printList();
+                //tempTag.printList();
                 tagParseArray.add(tempTag);
+                displayQuestion(questionNumber, mainQuestion, choiceOne, choiceTwo, choiceThree, choiceFour, tagArray, tagParseArray);
             }
         });
         choiceThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(NoobQuestionnaire.this, "Ayy lmao, this is a test for the onClick", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(NoobQuestionnaire.this, "Ayy lmao, this is a test for the onClick", Toast.LENGTH_SHORT).show();
                 choiceThree.setText("clicked!");
                 NQTag tempTag = (NQTag) v.getTag();
-                tempTag.printList();
+                //tempTag.printList();
                 tagParseArray.add(tempTag);
+                displayQuestion(questionNumber, mainQuestion, choiceOne, choiceTwo, choiceThree, choiceFour, tagArray, tagParseArray);
             }
         });
         choiceFour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(NoobQuestionnaire.this, "Ayy lmao, this is a test for the onClick", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(NoobQuestionnaire.this, "Ayy lmao, this is a test for the onClick", Toast.LENGTH_SHORT).show();
                 choiceFour.setText("clicked!");
                 NQTag tempTag = (NQTag) v.getTag();
-                tempTag.printList();
+                //tempTag.printList();
                 tagParseArray.add(tempTag);
+                displayQuestion(questionNumber, mainQuestion, choiceOne, choiceTwo, choiceThree, choiceFour, tagArray, tagParseArray);
 
             }
         });
 
    // mainQuestion.setText(nQuestionnaire.questionList.get(currentIndex).questionText);
+
+
     }
 
     @Override
@@ -166,6 +257,7 @@ public class NoobQuestionnaire extends AppCompatActivity implements AdapterView.
         //Toast.makeText(NoobQuestionnaire.this, "Ayy lmao, OITEMCLICK", Toast.LENGTH_SHORT).show();
     }
 
+    //sets up the text and the tags. Each case is a new question
     public void displayQuestion(int qNumber, TextView main, TextView one, TextView two, TextView three, TextView four,
                                 ArrayList<NQTag> questionTagArray, ArrayList<NQTag> parseArray){
         switch(qNumber){
@@ -174,19 +266,84 @@ public class NoobQuestionnaire extends AppCompatActivity implements AdapterView.
                 one.setText("I was picked to Play for a professional sports team");
                 one.setTag(questionTagArray.get(questionNumber * 4));
                 two.setText("When being picked for a team, I am always picked first");
-                one.setTag(questionTagArray.get(questionNumber * 4 + 1));
+                two.setTag(questionTagArray.get(questionNumber * 4 + 1));
                 three.setText("I have some athletic ability, but it isn't noteworthy");
-                one.setTag(questionTagArray.get(questionNumber * 4 + 2));
+                three.setTag(questionTagArray.get(questionNumber * 4 + 2));
                 four.setText("When being picked for a team, I'm always last");
-                one.setTag(questionTagArray.get(questionNumber * 4 + 3));
+                four.setTag(questionTagArray.get(questionNumber * 4 + 3));
                 questionNumber++;
                 break;
+            //TAGS 4,5
             case 1:
-                main.setText("Oh boy here we go, touching me a shit");
+                main.setText("It's better to be:");
+                one.setText("Graceful");
+                one.setTag(questionTagArray.get(4));
+                two.setText("Tough");
+                two.setTag(questionTagArray.get(5));
+                three.setVisibility(three.INVISIBLE);
+                four.setVisibility(three.INVISIBLE);
+                questionNumber++;
+                break;
+            //TAGS 6,7
+            case 2:
+                main.setText("Its better to have");
+                one.setText("Great Strength");
+                one.setTag(questionTagArray.get(6));
+                two.setText("Great Stamina");
+                two.setTag(questionTagArray.get(7));
+                questionNumber++;
+                break;
+            //tags 7,8
+            case 3:
+                main.setText("Its better to be");
+                one.setText("Physically Powerful");
+                one.setTag(questionTagArray.get(7));
+                two.setText("Physically Agile");
+                two.setTag(questionTagArray.get(7));
+                questionNumber++;
+                break;
+            //tags 9.10
+            case 4:
+                main.setText("It's worse to be:");
+                one.setText("Unliked");
+                one.setTag(questionTagArray.get(9));
+                two.setText("Weak");
+                two.setTag(questionTagArray.get(10));
+                questionNumber++;
+                break;
+            //tags 11, 12
+            case 5:
+                main.setText("It's worse to be:");
+                one.setText("Too Skinny");
+                one.setTag(questionTagArray.get(11));
+                two.setText("Too Stocky");
+                two.setTag(questionTagArray.get(12));
+                questionNumber++;
+                break;
+            //tags 13, 14
+            case 6:
+                main.setText("It's better to be:");
+                one.setText("Smart");
+                one.setTag(questionTagArray.get(13));
+                two.setText("Strong");
+                two.setTag(questionTagArray.get(14));
+                questionNumber++;
+                break;
+            //tags 15, 16
+            case 7:
+                main.setText("The wilderness is a nice place to visit, but I wouldn't want to live there.");
+                one.setText("True");
+                one.setTag(questionTagArray.get(15));
+                two.setText("False");
+                two.setTag(questionTagArray.get(16));
+                questionNumber++;
                 break;
             default:
                 break;
         }
+        //for(int a = 0; a < parseArray.size(); a++){
+            //parseArray.get(a).printList();
+        //}
     }
 
 }
